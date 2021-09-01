e365728b7c63a95cd8b8692dbc0a53f157d6274f
kimberly lin <kimberly.lin10@gmail.com>
2021-08-31 17:16:43 -0700

    completed functions that append imgs from giphy API to webpage. Added event listener to clear existing imgs

--- app.js
+++ app.js
@@ -18,19 +18,37 @@ const $giphyInput = $("#giphy__input");
 const $giphyForm = $("#giphy__form");
 
 const GIPHY_URL = "http://api.giphy.com/v1/gifs/search";
-//?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
-
-let searchPrompt;
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
 /** Takes in search prompt and then append search prompt to the url as query string
  * Return response data from api
  */
-
-async function giphyApi() {
-  const res = await axois.get(``);
+async function giphyApi(searchPrompt) {
+  const res = await axios.get(GIPHY_URL, { params: { q: searchPrompt, api_key: API_KEY } });
+  let imgURL= res.data.data[0].images.original.url;
+  return imgURL;
 }
 
-$giphyForm.on("submit", function (e) {
-  e.preventDefault();
-  searchPrompt = $giphyInput.val();
-});
+/** Input an embedded URL and creates img element in DOM, then append.*/
+function makeIMG(url) {
+  const $img = $("<img>").attr("src",url);
+  $imgDiv.append($img);
+}
+
+/** Capturing the form submission input and running the API based on input. */
+$giphyForm.on("submit", submitEvent);
+
+async function submitEvent(e){
+    e.preventDefault();
+    const searchPrompt = $giphyInput.val();
+    let URL = await giphyApi(searchPrompt);// put await here to make sure code waits before executing next line
+    makeIMG(URL);
+}
+
+/** Make event listener to remove all images stored in $imgDiv */
+$("#giphy__remove").on("click", removeIMG);
+
+function removeIMG(){
+  $imgDiv.empty();
+  console.log("remove button has been clicked")
+}
